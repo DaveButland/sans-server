321bf32910198f111b114e42ed781e00b94c2e51
David Butland <dgbutland@gmail.comm>
2019-08-24 14:09:02 +0100

    Posting contact form now works

--- src/messages.js
+++ src/messages.js
@@ -5,32 +5,23 @@ const response = require('./response');
 
 //create a new message
 exports.create = (event, context, callback) => {
-
-	if ( ( !event.headers.Authorization ) || ( !event.headers.Authorization.startsWith("Bearer ") ) ) {
-		return response.invalid( { errorMessage: "Missing or Invalid Authorization Token" } );
-	}
-
-	const sub = validateJWT.getSub( event.headers.Authorization.slice(7) ) ;
-
-	if ( !sub ) {
-		callback( null, response.failure( { errorMessages: "Invalid Token"} ) ) ;
-		return false ;
-	}
 	
 	const body = JSON.parse( event.body ) ;
 	const table = 'sans-messages' ;
+	const quyen = '832bb986-871d-4bd2-a832-9e7134265604' ; // hard coded for now
+
 	var message = {} ;
-	message.userid    = sub ;
+	message.userid    = quyen ;
 	message.messageid = uuid.v4() ;
 	message.name      = body.name ;
-	message.subject   = body.subject ;
-	message.start     = body.start ;
-	message.end       = body.end ; //calculated
+	message.email     = body.email ;
+	message.portfolio = body.portfolio ;
+	message.date      = body.date ;
+	message.time      = body.time ;
 	message.duration  = body.duration ;
 	message.type      = body.type ;
 	message.location  = body.location ;
-	message.portfolio = body.portfolio ;
-	message.email     = body.email ;
+	message.subject   = body.subject ;
 	message.content   = body.content ;
 	message.createdAt = Date.now() ;
 	
